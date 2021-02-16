5f07932d1a43a707809e42450789dfe7506bf95b
cky1312 <cky13129@gmail.com>
2021-02-16 22:37:20 +0530

    updated

--- Assignment_2/Fibonacci.java
+++ Assignment_2/Fibonacci.java
@@ -1,5 +1,18 @@
 package Assignment_2;
 
+import java.util.*;
 public class Fibonacci {
-    
+
+    static int Fibonacci(int n){
+        if(n <= 1)
+            return 1;
+        return (Fibonacci(n - 1) +  Fibonacci(n - 2));
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        System.out.println(Fibonacci(n));
+    }
 }
