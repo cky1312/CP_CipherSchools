40840237dd8aff0e955d2461a7def0920c2ff282
cky1312 <cky13129@gmail.com>
2021-02-16 23:16:34 +0530

    merge two sorted array

--- Assignment_1/Merge2SortedArray.java
+++ Assignment_1/Merge2SortedArray.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class Merge2SortedArray {
+package Assignment_1;
+
+import java.util.*;
+public class Merge2SortedArray {
+
+    static void sorted(int arr[], int brr[],ArrayList<Integer> crr, int n, int m){
+        int i = 0, j = 0;
+        while(i < n && j < m){
+            if(arr[i] < brr[j]){
+                crr.add(arr[i]);
+                i++;
+            }
+            else{
+                crr.add(brr[j]);
+                j++;
+            }
+        }
+
+        while(i < n){
+            crr.add(arr[i]);
+            i++;
+        }
+        while(j < m){
+            crr.add(brr[j]);
+            j++;
+        }
+        for (Integer integer : crr) {
+            System.out.print(integer+" ");
+        }
+        // return crr;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array 1");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of 1st array");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+
+        System.out.println("Enter the size of the array 2");
+        int m = sc.nextInt();
+        int brr[] = new int[m];
+        System.out.println("Enter the elements of the 2nd array");
+        for(int i = 0; i < m; i++){
+            brr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        Arrays.sort(brr);
+        ArrayList<Integer> crr = new ArrayList<>(n + m);
+        sorted(arr, brr,crr, n, m);
+
+        sc.close();
+    }
 }
--- Assignment_2/Fibonacci.java
+++ Assignment_2/Fibonacci.java
@@ -5,7 +5,7 @@ public class Fibonacci {
 
     static int Fibonacci(int n){
         if(n <= 1)
-            return 1;
+            return n;
         return (Fibonacci(n - 1) +  Fibonacci(n - 2));
     }
     public static void main(String[] args) {
