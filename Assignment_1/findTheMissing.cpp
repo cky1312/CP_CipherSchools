1d57196628e9b58274d204537a95cef03a68e157
cky1312 <cky13129@gmail.com>
2021-02-16 10:09:41 +0530

    ..

--- Assignment_1/findTheMissing.cpp
+++ Assignment_1/findTheMissing.cpp
@@ -7,12 +7,12 @@ int findMissing(vector<int> vec, int n){
     for(int i = 0; i < n; i++){
         sum += vec[i];
     }
-    return (totalSum - sum);
+    return totalSum - sum;
 }
 
 int main(){
     vector<int> vec = {1,2,3,4,5,6, 8, 9};
-    int n = vec.size() + 1;
+    int n = vec.size() + 1 ;
 
     int missingElement = findMissing(vec,n);
     cout<<missingElement;
