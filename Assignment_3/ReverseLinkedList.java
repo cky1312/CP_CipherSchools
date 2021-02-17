e0385f0ea02cee719d354385c914e5e62756097a
cky1312 <cky13129@gmail.com>
2021-02-17 21:35:31 +0530

    update

--- Assignment_3/ReverseLinkedList.java
+++ Assignment_3/ReverseLinkedList.java
@@ -4,21 +4,22 @@ import java.util.*;
 public class ReverseLinkedList {
 
     static Node head;
-    static class Node{
+
+    static class Node {
         int data;
         Node next;
 
-        Node(int d){
+        Node(int d) {
             data = d;
             next = null;
         }
     }
 
-    Node reverse(Node n){
+    Node reverse(Node n) {
         Node prev = null;
         Node current = n;
         Node next = null;
-        while(current != null){
+        while (current != null) {
             next = current.next;
             current.next = prev;
             prev = current;
@@ -28,21 +29,21 @@ public class ReverseLinkedList {
         return n;
     }
 
-    void printList(Node n){
-        while(n != null){
-            System.out.print(n.data+" ");
+    void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
             n = n.next;
         }
     }
-    
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
 
         ReverseLinkedList list = new ReverseLinkedList();
-        list.head = new Node(85);
-        list.head.next = new Node(15);
-        list.head.next.next = new Node(4);
-        list.head.next.next.next = new Node(20);
+        ReverseLinkedList.head = new Node(85);
+        ReverseLinkedList.head.next = new Node(15);
+        ReverseLinkedList.head.next.next = new Node(4);
+        ReverseLinkedList.head.next.next.next = new Node(20);
  
         System.out.println("Given Linked list");
         list.printList(head);
@@ -51,7 +52,7 @@ public class ReverseLinkedList {
         System.out.println("Reversed linked list ");
         list.printList(head);
 
-        sc.close();
+        // sc.close();
     }
     
 }
