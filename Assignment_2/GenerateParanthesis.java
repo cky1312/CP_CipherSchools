4750d0f1cc79601330b520c688633b45873dee99
cky1312 <cky13129@gmail.com>
2021-02-17 19:06:45 +0530

    Assignment3

---
+++ Assignment_2/GenerateParanthesis.java
@@ -0,0 +1,12 @@
+package Assignment_2;
+
+import java.util.*;
+public class GenerateParanthesis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}
