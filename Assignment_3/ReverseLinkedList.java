56098bafa109ef7646aee5d4b14ab84db4050cdf
cky1312 <cky13129@gmail.com>
2021-02-17 19:26:53 +0530

    .

--- Assignment_3/ReverseLinkedList.java
+++ Assignment_3/ReverseLinkedList.java
@@ -3,7 +3,10 @@ package Assignment_3;
 import java.util.*;
 public class ReverseLinkedList {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
     }
     
 }
